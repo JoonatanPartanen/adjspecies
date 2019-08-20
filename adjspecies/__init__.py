2e28ac44412e00f7ba7acae5326744fab394f821
Joonatan Partanen <joonatan.partanen@aalto.fi>
2019-08-20 20:35:16 +0300

    Generalize word lists, fix verbs file syntax, add resume for generation without replacement

--- adjspecies/__init__.py
+++ adjspecies/__init__.py
@@ -6,26 +6,29 @@ import yaml
 
 class AnimalTokens:
     def __init__(self,
-                 adjective_file='adjectives.yaml',
-                 animal_file='animals_short.yaml',
-                 with_replacement=False):
+                 word_list_files=('adjectives.yaml',
+                                  'animals_short.yaml',
+                                  'verbs.yaml'),
+                 with_replacement=False,
+                 previously_generated=set()):
         this_folder = Path(os.path.dirname(os.path.abspath(__file__)))
-        adjective_abs = this_folder / adjective_file
-        animals_abs = this_folder / animal_file
-        with open(adjective_abs, 'r') as f:
-            self.adjectives = yaml.safe_load(f)
-        with open(animals_abs, 'r') as f:
-            self.animals = yaml.safe_load(f)
-        self.generated = set()
+        self.word_lists = []
+        for file_path in word_list_files:
+            processed_path = Path(file_path)
+            if not processed_path.is_absolute():
+                processed_path = this_folder / processed_path
+            with open(processed_path, 'r') as f:
+                self.word_lists.append(yaml.safe_load(f))
+        self.previously_generated = previously_generated
         self.with_replacement = with_replacement
 
-    def random_token(self, sep='', maxlen=16):
+    def random_token(self, sep='', maxlen=21):
         while True:
-            token = choice(self.adjectives) + sep + choice(self.animals)
+            token = sep.join(choice(lst) for lst in self.word_lists)
             if maxlen and len(token) > maxlen:
                 continue
-            if not self.with_replacement and token in self.generated:
+            if not self.with_replacement and token in self.previously_generated:
                 continue
             break
-        self.generated.add(token)
+        self.previously_generated.add(token)
         return token
